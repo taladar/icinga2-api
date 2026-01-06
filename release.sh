ea4aaec6fa94eb211f08db805b38453cfb4f4139
Matthias Hörmann <matthias.hoermann@saltation.com>
2026-01-06 10:06:56 +0100

    feat(release): Add release.sh and cliff.toml
    Ran_pre-commit_check: markdown:::rumdl
    Ran_pre-commit_check: shell:::shellcheck
    Ran_pre-commit_check: shell:::shfmt
    Ran_pre-commit_check: toml:::taplo
    Ran_pre-commit_check: common:::whitespace
    Ran_pre-commit_check: common:::non-ascii-filenames
    Ran_pre-commit_check: common:::typos
    Ran_pre-commit_check: rust:::cargo-check
    Ran_pre-commit_check: rust:::cargo-deny-advisories
    Ran_pre-commit_check: rust:::cargo-deny-licenses
    Ran_pre-commit_check: rust:::cargo-deny-sources
    Ran_pre-commit_check: rust:::cargo-sort
    Ran_pre-commit_check: rust:::changelog-md-includes-version
    Ran_pre-commit_check: rust:::clippy
    Ran_pre-commit_check: rust:::crate-version-not-in-cargo-lock
    Ran_pre-commit_check: rust:::doc
    Ran_pre-commit_check: rust:::formatting
    Ran_pre-commit_check: rust:::lib:::semver-violations
    Ran_pre-commit_check: rust:::nextest
    Ran_pre-commit_check: rust:::unused-dependencies

---
+++ release.sh
@@ -0,0 +1,33 @@
+#!/bin/bash
+
+set -e -u
+
+if [[ $# -lt 1 ]]; then
+  echo "Usage: $0 <major|minor|patch>" >&2
+  exit 1
+fi
+
+level="$1"
+
+cargo set-version --bump "${level}"
+
+version="$(cargo get package.version)"
+
+git cliff --prepend CHANGELOG.md -u -t "icinga2_api_${version}"
+
+rumdl fmt --fix CHANGELOG.md
+
+cargo build
+
+git add CHANGELOG.md Cargo.toml
+
+git commit -m "chore(release): Release version ${version}"
+
+git tag "icinga2_api_${version}"
+
+for remote in origin github; do
+  git push "${remote}"
+  git push "${remote}" "icinga2_api_${version}"
+done
+
+cargo publish --dry-run
